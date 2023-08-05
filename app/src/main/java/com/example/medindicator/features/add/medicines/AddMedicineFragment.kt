535af8b99fd1544807676dd969a1fbd04e316dc3
Anderson Chaves <108032363+chaavez@users.noreply.github.com>
2023-08-04 21:04:16 -0300

    Added bottom bar navigation in project

---
+++ app/src/main/java/com/example/medindicator/features/add/medicines/AddMedicineFragment.kt
@@ -0,0 +1,10 @@
+package com.example.medindicator.features.add.medicines
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class AddMedicineFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
