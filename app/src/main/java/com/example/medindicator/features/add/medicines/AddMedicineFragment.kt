11b1aeba8251bee1d11416a59059b2748b00fbb0
Anderson Chaves <108032363+chaavez@users.noreply.github.com>
2023-08-19 23:01:45 -0300

    Added the fill boxes for creating a new medication alarm

--- app/src/main/java/com/example/medindicator/features/add/medicines/AddMedicineFragment.kt
+++ app/src/main/java/com/example/medindicator/features/add/medicines/AddMedicineFragment.kt
@@ -5,10 +5,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.medindicator.R
 import com.example.medindicator.databinding.FragmentAddMedicineBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.textfield.TextInputEditText
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
@@ -30,6 +35,11 @@ class AddMedicineFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupMenuItem()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setupDropDownMenu()
         setupDatePicker()
     }
 
@@ -42,22 +52,33 @@ class AddMedicineFragment : Fragment() {
     private fun setupDatePicker() {
         val myCalendar = Calendar.getInstance()
 
-        val datePicker = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+        val startDatePicker = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
             myCalendar.set(Calendar.YEAR, year)
             myCalendar.set(Calendar.MONTH, month)
             myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-            updateLabel(myCalendar)
+            updateLabel(binding.startDateTextInputEditText, myCalendar)
         }
 
-        binding.dateTextInputEditText.setOnClickListener {
-            DatePickerDialog(requireContext(), datePicker, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+        binding.startDateTextInputEditText.setOnClickListener {
+            DatePickerDialog(requireContext(), startDatePicker, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
                 myCalendar.get(Calendar.DAY_OF_MONTH)).show()
         }
     }
 
-    private fun updateLabel(myCalendar: Calendar) {
+    private fun updateLabel(dateTextInputEditText: TextInputEditText, myCalendar: Calendar) {
         val myFormat = "dd/MM/yyyy"
         val dateFormat = SimpleDateFormat(myFormat, Locale.getDefault())
-        binding.dateTextInputEditText.setText(dateFormat.format(myCalendar.time))
+        dateTextInputEditText.setText(dateFormat.format(myCalendar.time))
+    }
+
+    private fun setupDropDownMenu() {
+        val selectItems = listOf("1 dia", "3 dias", "7 dias", "15 dias")
+        val adapter = ArrayAdapter(requireContext(), R.layout.dropdown_item_list, selectItems)
+        binding.endAutoCompleteTextView.setAdapter(adapter)
+
+        binding.endAutoCompleteTextView.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, i, l ->
+            val itemSelected = adapterView.getItemAtPosition(i)
+            Toast.makeText(requireContext(), "item $itemSelected", Toast.LENGTH_SHORT).show()
+        }
     }
 }
