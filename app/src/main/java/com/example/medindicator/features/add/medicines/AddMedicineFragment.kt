926aeea792ccb476d55f54b0389cb048721dccf8
Anderson Chaves <108032363+chaavez@users.noreply.github.com>
2023-08-22 16:22:19 -0300

    Added data filling logic for creating a new card

--- app/src/main/java/com/example/medindicator/features/add/medicines/AddMedicineFragment.kt
+++ app/src/main/java/com/example/medindicator/features/add/medicines/AddMedicineFragment.kt
@@ -2,13 +2,13 @@ package com.example.medindicator.features.add.medicines
 
 import android.app.DatePickerDialog
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Adapter
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.medindicator.R
 import com.example.medindicator.databinding.FragmentAddMedicineBinding
@@ -16,6 +16,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.textfield.TextInputEditText
 import java.text.SimpleDateFormat
 import java.util.Calendar
+import java.util.Date
 import java.util.Locale
 
 
@@ -35,12 +36,9 @@ class AddMedicineFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupMenuItem()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        setupDropDownMenu()
         setupDatePicker()
+        setupDropDownMenu()
+        setupNextButton()
     }
 
     private fun setupMenuItem() {
@@ -51,7 +49,6 @@ class AddMedicineFragment : Fragment() {
 
     private fun setupDatePicker() {
         val myCalendar = Calendar.getInstance()
-
         val startDatePicker = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
             myCalendar.set(Calendar.YEAR, year)
             myCalendar.set(Calendar.MONTH, month)
@@ -60,8 +57,13 @@ class AddMedicineFragment : Fragment() {
         }
 
         binding.startDateTextInputEditText.setOnClickListener {
-            DatePickerDialog(requireContext(), startDatePicker, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
-                myCalendar.get(Calendar.DAY_OF_MONTH)).show()
+            DatePickerDialog(
+                requireContext(),
+                startDatePicker,
+                myCalendar.get(Calendar.YEAR),
+                myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
         }
     }
 
@@ -71,14 +73,86 @@ class AddMedicineFragment : Fragment() {
         dateTextInputEditText.setText(dateFormat.format(myCalendar.time))
     }
 
+    private fun parseDate(dateString: String): Date {
+        val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        return dateFormat.parse(dateString) ?: Date()
+    }
+
+    private fun extractDaysFromSelectedItem(selectedItem: String): Int {
+        val regex = Regex("""(\d+) dias?""").find(selectedItem)
+        return regex?.groupValues?.getOrNull(1)?.toInt() ?: 0
+    }
+
     private fun setupDropDownMenu() {
+        binding.endAutoCompleteTextView.isEnabled = false
+        binding.endDateTextInputLayout.isEnabled = false
         val selectItems = listOf("1 dia", "3 dias", "7 dias", "15 dias")
         val adapter = ArrayAdapter(requireContext(), R.layout.dropdown_item_list, selectItems)
         binding.endAutoCompleteTextView.setAdapter(adapter)
 
-        binding.endAutoCompleteTextView.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, i, l ->
-            val itemSelected = adapterView.getItemAtPosition(i)
-            Toast.makeText(requireContext(), "item $itemSelected", Toast.LENGTH_SHORT).show()
+        binding.startDateTextInputEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                val startDateText = p0.toString()
+                val startDateFilled = startDateText.isNotEmpty()
+                binding.endAutoCompleteTextView.isEnabled = startDateFilled
+                binding.endDateTextInputLayout.isEnabled = startDateFilled
+            }
+        })
+
+        binding.endAutoCompleteTextView.onItemClickListener =
+            AdapterView.OnItemClickListener { adapterView, view, i, l ->
+                val itemSelected = adapterView.getItemAtPosition(i) as String
+                val daysToAdd = extractDaysFromSelectedItem(itemSelected)
+                val initialDateString = binding.startDateTextInputEditText.text.toString()
+                val initialDate = parseDate(initialDateString)
+
+                val calendar = Calendar.getInstance()
+                calendar.time = initialDate
+                val dateList = mutableListOf<Date>()
+
+                for (day in 1..daysToAdd) {
+                    dateList.add(calendar.time)
+                    calendar.add(Calendar.DAY_OF_MONTH, 1)
+                }
+            }
+    }
+
+    private fun setupNextButton() {
+        binding.nextButton.isEnabled = allFieldsFilled()
+        binding.medicineTextInputEditText.addTextChangedListener(textWatcher())
+        binding.dosageTextInputEditText.addTextChangedListener(textWatcher())
+        binding.startDateTextInputEditText.addTextChangedListener(textWatcher())
+        binding.endAutoCompleteTextView.addTextChangedListener(textWatcher())
+
+        binding.nextButton.setOnClickListener {
+            //findNavController().navigate()
+        }
+    }
+
+    private fun allFieldsFilled(): Boolean {
+        val medicineText = binding.medicineTextInputEditText.text.toString()
+        val dosageText = binding.dosageTextInputEditText.text.toString()
+        val startDateText = binding.startDateTextInputEditText.text.toString()
+        val endDateText = binding.endAutoCompleteTextView.text.toString()
+        return medicineText.isNotEmpty() && dosageText.isNotEmpty()
+                && startDateText.isNotEmpty() && endDateText.isNotEmpty()
+    }
+
+    private fun textWatcher(): TextWatcher {
+        return object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(p0: Editable?) {
+                binding.nextButton.isEnabled = allFieldsFilled()
+            }
         }
     }
 }
